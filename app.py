70fbbc6d7b5f5a6b0ac8121d214672d92ac0141f
erhopf <erhopf@microsoft.com>
2019-01-04 08:19:55 -0800

    Fixed by setting up sentiment as a POST request.

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ if sys.version_info.major < 3:
 sys.setdefaultencoding('utf-8')
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 
 @app.route('/')
 def index():
@@ -12,25 +13,34 @@ def index():
 
 @app.route('/translate-text')
 def translate_text():
-    text_input = request.args.get("text", default="", type=str)
-    translation_output = request.args.get("to", default="", type=str)
+    text_input = request.args.get('text', default='', type=str)
+    translation_output = request.args.get('to', default='', type=str)
     response = translate.get_translation(text_input, translation_output)
     return jsonify(response)
 
 @app.route('/text-to-speech')
 def text_to_speech():
-    text_input = request.args.get("text", default="", type=str)
-    voice_font = request.args.get("voice", default="", type=str)
+    text_input = request.args.get('text', default='', type=str)
+    voice_font = request.args.get('voice', default='', type=str)
     tts = synthesize.TextToSpeech(text_input, voice_font)
     tts.get_token()
     audio_response = tts.save_audio()
     return audio_response
 
-@app.route('/sentiment-analysis')
+@app.route('/sentiment-analysis', methods=['POST'])
 def sentiment_analysis():
-    input_text = request.args.get("input", type=str)
-    input_language = request.args.get("inlang", type=str)
-    output_text = request.args.get("output", type=str)
-    output_language = request.args.get("outlang", type=str)
-    response = sentiment.get_sentiment(input_text, input_language, output_text, output_language)
+    #input_text = request.args.get('input', type=str)
+    #input_language = request.args.get('inlang', type=str)
+    #output_text = request.args.get('output', type=str)
+    #output_language = request.args.get('outlang', type=str)
+    data = request.get_json()
+    input_text = data['inputText']
+    input_lang = data['inputLanguage']
+    output_text = data['outputText']
+    output_lang =  data['outputLanguage']
+    #return jsonify({'inputText' : input_text, 'inputLanguage': input_lang, 'outputText': output_text, 'outputLanguage': output_lang })
+    response = sentiment.get_sentiment(input_text, input_lang, output_text, output_lang)
     return jsonify(response)
+
+if __name__ == '__main__':
+    app.run()
--- static/scripts/main.js
+++ static/scripts/main.js
@@ -55,20 +55,40 @@ $(function() {
     var inputLanguage = document.getElementById("detected-language-result").innerHTML;
     var outputText = document.getElementById("translation-result").value;
     var outputLanguage = document.getElementById("select-language").value;
-    console.log(outputLanguage);
+
+    var sentimentRequest = { "inputText": inputText, "inputLanguage": inputLanguage, "outputText": outputText, "outputLanguage": outputLanguage };
+
     if (inputText !== "") {
       $.ajax({
-        url: '/sentiment-analysis?input=' + inputText + '&inlang=' + inputLanguage + '&output=' + outputText + '&outputlang=' + outputLanguage,
-        method: 'get',
+        url: '/sentiment-analysis',
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json'
+        },
+        dataType: 'json',
+        data: JSON.stringify(sentimentRequest),
         success: function(data) {
-          console.log(data);
-          document.getElementById("input-sentiment").textContent = data.documents[0].score;
-          if (typeof data.documents[1] !== 'undefined') {
-            document.getElementById("translation-sentiment").textContent = data.documents[1].score;
-          } else{
-            document.getElementById("translation-sentiment").textContent = "Sentiment analysis isn't supported for " + document.getElementById("select-language").value + "."
+          for (var i = 0; i < data.documents.length; i++) {
+            if (typeof data.documents[i] !== 'undefined'){
+              if (data.documents[i].id === "1") {
+                document.getElementById("input-sentiment").textContent = data.documents[i].score;
+              }
+              if (data.documents[i].id === "2") {
+                document.getElementById("translation-sentiment").textContent = data.documents[i].score;
+              }
+            }
           }
-          if (document.getElementById("input-sentiment").textContent !== '' && document.getElementById("translation-result").textContent !== ''){
+          for (var i = 0; i < data.errors.length; i++) {
+            if (typeof data.errors[i] !== 'undefined'){
+              if (data.errors[i].id === "1") {
+                document.getElementById("input-sentiment").textContent = data.errors[i].message;
+              }
+              if (data.errors[i].id === "2") {
+                document.getElementById("translation-sentiment").textContent = data.errors[i].message;
+              }
+            }
+          }
+          if (document.getElementById("input-sentiment").textContent !== '' && document.getElementById("translation-sentiment").textContent !== ''){
             document.getElementById("sentiment").style.display = "block";
           }
         }
